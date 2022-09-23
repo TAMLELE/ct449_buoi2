100a48fdb78a863de300b46e759227998f7491a9
TAMLELE <104675352+TAMLELE@users.noreply.github.com>
2022-09-23 15:08:17 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, net) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal serveer Error",
+    });
+});
 
 
 app.get("/", (req, res)=>{
