d48e4dd70de235d93d35afa368364dd5a7b7be33
TAMLELE <104675352+TAMLELE@users.noreply.github.com>
2022-09-23 14:29:28 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = this.require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
