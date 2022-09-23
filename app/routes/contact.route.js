398f0a64ae694be0add6ed50db3fb61270785e90
TAMLELE <104675352+TAMLELE@users.noreply.github.com>
2022-09-23 14:18:36 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -1,18 +1,19 @@
-const express = require ( "express" ) ;
-const contacts = require ( " ../controllers/contact.controller");
+const express = require ("express");
+const contacts = require ("../controllers/contact.controller");
 
-const router = express . Router ();
+const router = express.Router();
 
-router.route ( " / " )
-    .get ( contacts . findAll )
-    .post ( contacts.create )
-    .delete ( contacts.deleteAll ) ;
+router.route("/")
+.get(contacts.findALL)
+.post(contacts.create)
+.delete(contacts.deleteALL);
 
-router.route ( " / favorite " )
-    .get ( contacts . findAllFavorite ) ;
-router.route ( " / : id " )
-    .get ( contacts.findOne ) 
-    .put ( contacts.update ) 
-    .delete ( contacts.delete ) ; 
+router.route("/favorite")
+.get(contacts.findALLFavorite);
 
-module.exports = router ;
+router.route("/:id")
+.get(contacts.findOne)
+.put(contacts.update)
+.delete(contacts.delete);
+
+module.exports = router;
