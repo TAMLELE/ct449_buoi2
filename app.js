87e2b58f04e34aeca34c0afcd3261783dab76c2a
TAMLELE <104675352+TAMLELE@users.noreply.github.com>
2022-09-30 18:10:25 +0700

    cai dat cac henld CSDL

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const contactsRouter = require("./app/routes/contact.route");
-// const ApiError = require("./app/api-error");
+
+const ApiError = require('./app/api-error');
+const router = express.Router()
+
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/contacts", contactsRouter);
-// app.use((req, res, net) => {
-    // return next(new ApiError(404, "Resource not found"));
-// });
-// app.use((err, req, res, next) => {
-//     return res.status(error.statusCode || 500).json({
-//         message: error.message || "Internal serveer Error",
-//     });
-// });
 
-
-
-app.get("/", (req, res)=>{
-    res.json({ message: " Welcome to contact book application."});
+app.use((req, res, next) => {
+    
+   return next(new ApiError(404, "Resource not found"));
 });
 
 
+app.use((error, req, res, next) => {
+
+    return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+    });
+});
+
+app.get('/', (req, res) =>{
+    res.json({message:"welcome to contact book application."});
+});
 
 module.exports = app;
